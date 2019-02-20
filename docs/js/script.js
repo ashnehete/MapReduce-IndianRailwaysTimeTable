2ddf9019edecf119e512fb34e15ad026aa6d2a2f
Aashish Nehete <ashnehete@gmail.com>
2019-02-20 22:17:44 +0530

    Completed docs, added tables

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,30 @@
+Papa.parse('formatted_output/TotalDistance.csv', {
+    download: true,
+    header: true,
+    dynamicTyping: true,
+    complete: (results) => {
+        renderTable(t1, results.data, results.meta.fields)
+    }
+})
+
+Papa.parse('formatted_output/TotalTrains.csv', {
+    download: true,
+    header: true,
+    dynamicTyping: true,
+    complete: (results) => {
+        renderTable(t2, results.data, results.meta.fields)
+    }
+})
+
+Papa.parse('formatted_output/Frequency.csv', {
+    download: true,
+    header: true,
+    dynamicTyping: true,
+    complete: (results) => {
+        renderTable(t3, results.data, results.meta.fields)
+    }
+})
+
 Papa.parse('formatted_output/DistanceDistribution.csv', {
     download: true,
     header: true,
@@ -9,8 +36,8 @@ Papa.parse('formatted_output/DistanceDistribution.csv', {
             if (!data[bin]) data[bin] = 0;
             data[bin] = data[bin] + 1;
         }
-        console.log(data)
         renderHistogram(data)
+        // renderTable(t4, results.data, results.meta.fields) // Becomes too big
     }
 })
 
@@ -23,7 +50,7 @@ function renderHistogram(data) {
             datasets: [{
                 label: 'Frequency',
                 data: data,
-                backgroundColor: 'rgba(238,77,90,0.5)',
+                backgroundColor: 'rgba(238,77,90,0.5)'
             }]
         },
         options: {
@@ -56,6 +83,24 @@ function keys(data) {
     for (let i in keys) {
         keys[i] = parseInt(keys[i]) * 100
     }
-    console.log(keys)
+    keys.push(4300)
     return keys
+}
+
+function renderTable(element, data, columns) {
+    let htmlText = '<table><thead><tr>'
+    for (let i of columns) {
+        htmlText += `<th>${i}</th>`
+    }
+    htmlText += '</tr></thead><tbody>'
+    for (let i of data) {
+        // htmlText += '<tr><td>' + i.distance + '</td><td>' + i.distribution + '</td></tr>'
+        htmlText += '<tr>'
+        for (let j in i) {
+            htmlText += `<td>${i[j]}</td>`
+        }
+        htmlText += '</tr>'
+    }
+    htmlText += '</tbody></table>'
+    element.innerHTML = htmlText
 }
